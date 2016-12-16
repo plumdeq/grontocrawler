a0e35ba0c28a799084f6bef7722324a3da959f99
Asan Agibetov <asan@plumdeq.xyz>
2016-12-16 16:21:35 +0100

    minor: updates version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='grontocrawler',
-      version='0.3',
+      version='0.3.1',
       description='transformation of OWL ontologies into graphs',
       author='Asan Agibetov',
       author_email='asan.agibetov@gmail.com',
